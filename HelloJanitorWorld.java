e5e4e9075e2a4882637a0f17b91e24994d7e334d
smonitor <40175628+smonitor@users.noreply.github.com>
2021-07-31 16:04:01 +0200

    Add files via upload

---
+++ HelloJanitorWorld.java
@@ -0,0 +1,20 @@
+// ************************************************** 
+//     File:	HelloJanitorWorld.java
+//     Project:	Monitor (Server-/Start-/System-) 
+//     Content:	"Hello World"
+//     Copyr.©:	ITS SARL -- www.jes.it
+//              http://smolka.lima-city.de/
+// ************************************************** 
+
+package it.jes.monitor;
+
+public class HelloJanitorWorld {
+
+	/* @param args */
+	public static void main(String[] args) {
+		System.out.println("Hello Janitor World !");
+		System.out.println("Here is SMonitor 4 U.");
+		System.out.println(";)");
+	}
+
+}
